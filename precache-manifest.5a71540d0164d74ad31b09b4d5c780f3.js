bfa0e3f1707222d19bde043f463f4ba2b61f8ca1
Yury Misiyevich <misiyevich@gmail.com>
2021-03-23 10:00:49 +0300

    build

---
+++ precache-manifest.5a71540d0164d74ad31b09b4d5c780f3.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8e52ac3bc4b15181650b16449e6b9f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9c9a97f146a91796ada2",
+    "url": "/portfolio/static/css/main.d2cf940b.chunk.css"
+  },
+  {
+    "revision": "fb55e7360323933d852f",
+    "url": "/portfolio/static/js/2.9d24355c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9d24355c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c9a97f146a91796ada2",
+    "url": "/portfolio/static/js/main.d567ebbd.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "123ca976eef7702cc985d60c27e661d9",
+    "url": "/portfolio/static/media/42.123ca976.jpg"
+  },
+  {
+    "revision": "76e722f4cc4f68216ce46c5ffafe5f28",
+    "url": "/portfolio/static/media/animation.76e722f4.jpg"
+  },
+  {
+    "revision": "9c12449881722eb677184274a3fb2f42",
+    "url": "/portfolio/static/media/calendar.9c124498.jpg"
+  },
+  {
+    "revision": "d486aa1e0f0d1c06725ae803840701e3",
+    "url": "/portfolio/static/media/cv2.d486aa1e.pdf"
+  },
+  {
+    "revision": "2b81d0521f59ea3af40be4552ef4f4ba",
+    "url": "/portfolio/static/media/domchat.2b81d052.jpg"
+  },
+  {
+    "revision": "fc4c9bbc3c026f26ceb937b905dbd71e",
+    "url": "/portfolio/static/media/form.fc4c9bbc.jpg"
+  },
+  {
+    "revision": "5d1c1cbddf1375678504252ceb0a066a",
+    "url": "/portfolio/static/media/inst.5d1c1cbd.jpg"
+  },
+  {
+    "revision": "19e64b698a6a82ffc4c9c8288229c963",
+    "url": "/portfolio/static/media/landing.19e64b69.jpg"
+  },
+  {
+    "revision": "3a849cf047a9c2698c0b321c029659d5",
+    "url": "/portfolio/static/media/lumber.3a849cf0.jpg"
+  },
+  {
+    "revision": "b6d1c91c473918335a29633237b4763c",
+    "url": "/portfolio/static/media/mailme.b6d1c91c.jpg"
+  },
+  {
+    "revision": "9d8d6f495f612e079afcee4947fc9622",
+    "url": "/portfolio/static/media/me.9d8d6f49.jpg"
+  },
+  {
+    "revision": "e0d4e385a4bfa2947c6a6d0c6407fb78",
+    "url": "/portfolio/static/media/music.e0d4e385.jpg"
+  },
+  {
+    "revision": "748bab8380c523e947aebc0e4637e1fc",
+    "url": "/portfolio/static/media/news.748bab83.jpg"
+  },
+  {
+    "revision": "5166346f59d71c5278531cdcb19754f2",
+    "url": "/portfolio/static/media/parser.5166346f.jpg"
+  },
+  {
+    "revision": "01e5763606aa71441b8838d8959d5c2a",
+    "url": "/portfolio/static/media/task.01e57636.jpg"
+  }
+]);
