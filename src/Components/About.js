2000ae17a936b88fe62d41fb45d3c102887ee47e
Yury Misiyevich <misiyevich@gmail.com>
2021-03-23 09:52:20 +0300

    feat: update 2021

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,13 +7,16 @@ const About = () => {
         <div className="main about"/>
         <div className="aboutMe">
           <h3>Yury Misiyevich</h3>
-          <p>Looking for a front-end developer position. I have completed several courses for last 2 years
-            include 0.5-year front-end course by The Rolling
-            Scopes (EPAM) and now am ready to apply my skills
-            and grow up as developer. Also:</p>
+          <p>Looking for a front-end developer position.
+            I’m looking for a front-end developer position.
+            I have commercial experience of front-end development,
+            early completed projects by myself and completed
+            several courses include 0.5-year front-end course by
+            The Rolling Scopes (EPAM). Also:</p>
           <ul>
+            <li>Working as front-end developer since nov 2020 till now</li>
             <li>Have worked for 5 years as civil engineer</li>
-            <li>Graduated from the university in the top 10 in faculty.</li>
+            <li>Graduated from the university in the top 7 in faculty.</li>
             <li>Live in Minsk, Belarus for 26 years</li>
             <li>Like photography, hockey, building</li>
           </ul>
